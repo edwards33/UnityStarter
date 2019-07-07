6a8ab5c90169303e35111a1deaa45875db4aea78
Edward <edylennyy@gmail.com>
2019-07-07 23:25:53 +0300

    initial

---
+++ Assets/Scripts/Welcome.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Welcome : MonoBehaviour {
+	
+	void Start () 
+	{
+		print("Hi User!"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
